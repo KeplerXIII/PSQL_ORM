9e1cfb38e4ab37d17d833fd4c0736b900f8ffbfa
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2022-10-13 00:45:17 +0300

    Update

--- main.py
+++ main.py
@@ -1,8 +1,99 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import json
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from config import DSN
+from models import create_tables, Publisher, Shop, Book, Stock, Sale
+
+# Наоплнитель базы
+def fill_base():
+    with open("base.json", "r") as base:
+        data = json.load(base)
+        for body in data:
+            if body['model'] == "publisher":
+                publisher = Publisher(name=body["fields"]["name"])
+                session.add(publisher)
+                session.commit()
+                print(publisher.id)
+        for body in data:
+            if body['model'] == "shop":
+                shop = Shop(name=body["fields"]["name"])
+                session.add(shop)
+                session.commit()
+                print(shop.id)
+        for body in data:
+            if body['model'] == "book":
+                book = Book(title=body["fields"]["title"], publisher_id=body["fields"]["id_publisher"])
+                session.add(book)
+                session.commit()
+                print(book.id)
+        for body in data:
+            if body['model'] == "stock":
+                stock = Stock(book_id=body["fields"]["id_book"], shop_id=body["fields"]["id_shop"], count=body["fields"]["count"])
+                session.add(stock)
+                session.commit()
+                print(stock.id)
+        for body in data:
+            if body['model'] == "sale":
+                sale = Sale(price=body["fields"]["price"], date_sale=body["fields"]["date_sale"],
+                            stock_id=body["fields"]["id_stock"], count=body["fields"]["count"])
+                session.add(sale)
+                session.commit()
+                print(sale.id)
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Создаём движок, не совсем разобрался, но звучит круто.
+engine = sqlalchemy.create_engine(DSN)
+
+# Создаём классы, тоесть таблицы.
+create_tables(engine)
+
+# Открываем сессию
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Наполняем базу из JSON файла
+fill_base()
+
+# Выполняем запрос
 
 
+
+#
+# course1 = Course(name='Python')
+# print(course1.id)
+#
+# session.add(course1)
+# session.commit()
+#
+# print(course1.id)
+#
+# print(course1)
+#
+# hw1 = Homework(number=1, description='простая домашняя работа', course=course1)
+# hw2 = Homework(number=2, description='сложная домашняя работа', course=course1)
+# session.add_all([hw1, hw2])
+#
+# for c in session.query(Homework).filter(Homework.description.like('%сложн%')).all():
+#     print(c)
+#
+# for c in session.query(Course).join(Homework.course).filter(Homework.number == 3).all():
+#     print(c)
+#
+# c2 = Course(name='Java')
+# session.add(c2)
+# session.commit()
+#
+# subq = session.query(Homework).filter(Homework.description.like('%сложн%')).subquery()
+# for c in session.query(Course).join(subq, Course.id == subq.c.course_id).all():
+#      print(c)
+#
+# session.query(Course).filter(Course.name == 'Java').update({'name': 'JavaScript'})
+# session.commit()
+#
+# session.query(Course).filter(Course.name == 'JavaScript').delete()
+# session.commit()
+#
+# for c in session.query(Course).all():
+#     print(c)
+
+session.close()
--- models.py
+++ models.py
@@ -56,8 +56,8 @@ class Sale(Base):
     __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
-    date_sale = sq.Column(sq.Date, nullable=False)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.DateTime, nullable=False)
     stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
@@ -70,3 +70,5 @@ class Sale(Base):
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+
